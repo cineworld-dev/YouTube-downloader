c51b4b8717d13155f41ed13db8bed0d05df2e7c6
cineworld-dev <streamflix212@gmail.com>
2025-06-20 19:57:13 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 // api/index.js
-import ytdl from 'ytdl-core';
+const ytdl = require('ytdl-core');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
-  }
+  };
 
   if (req.method !== 'GET' && req.method !== 'POST') {
     return res.status(405).json({
